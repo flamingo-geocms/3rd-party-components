f2891ffc63554dcc389e7b36f44bf7e753cf8136
Meine Toonen <toonen.meine@gmail.com>
2014-11-18 11:09:35 +0100

    Open documents in window with scrollbars. Removed trailing spaces

--- ro-tercera-component/IdentifyParser.js
+++ ro-tercera-component/IdentifyParser.js
@@ -1,4 +1,4 @@
-/* 
+/*
  * Copyright (C) 2012-2014 B3Partners B.V.
  *
  * This program is free software: you can redistribute it and/or modify
@@ -29,18 +29,18 @@ Ext.define("viewer.components.rotercera.IdentifyParser",{
     TERCERALAYERID:"Tercera",
 
     checkForIdentification: [],
-    
+
     wfsToWmsLayer: {},
-    
+
     terceraMapping: null,
-    
+
     //info placeholders
     info: "",
     commentaarInfo: "",
-    
+
     highlightImage: null,
     highlightedImage: null,
-    
+
     component: null,
     config: {
         domElement: null
@@ -48,26 +48,26 @@ Ext.define("viewer.components.rotercera.IdentifyParser",{
     constructor: function (conf,component){
         this.component = component;
         this.initConfig(conf);
-        
+
         this.checkForIdentification.push("identificatie");
         this.checkForIdentification.push("plangebied");
         this.checkForIdentification.push("bestemmingsplangebiedid");
-        
+
         this.wfsToWmsLayer['Besluitvlak_P'] = "PV:Besluitvlak";
         this.wfsToWmsLayer['Besluitgebied_P'] = "PV:Besluitgebied";
         this.wfsToWmsLayer['Besluitvlak_X'] = "XGB:Besluitvlak";
         this.wfsToWmsLayer['Besluitgebied_X'] = "XGB:Besluitgebied";
-        this.wfsToWmsLayer['Besluitvlak_A'] = "AMB:Besluitgebied";      
+        this.wfsToWmsLayer['Besluitvlak_A'] = "AMB:Besluitgebied";
         this.wfsToWmsLayer['Besluitgebied_A'] = "AMB:Besluitgebied";
-        this.wfsToWmsLayer["Bestemmingsplangebied"] = "BP:Bestemmingsplangebied"; 
-        this.wfsToWmsLayer["Wijzigingsplangebied"] = "BP:Wijzigingsplangebied"; 
-        this.wfsToWmsLayer["Enkelbestemming"] = "BP:Enkelbestemming"; 
-        this.wfsToWmsLayer["Figuur"] = "BP:Figuur"; 
-        this.wfsToWmsLayer["Lettertekenaanduiding"] = "BP:Lettertekenaanduiding"; 
-        this.wfsToWmsLayer["Maatvoering"] = "BP:Maatvoering"; 
-        this.wfsToWmsLayer["Dubbelbestemming"] = "BP:Dubbelbestemming"; 
-        this.wfsToWmsLayer["Bouwvlak"] = "BP:Bouwvlak"; 
-        this.wfsToWmsLayer["Gebiedsaanduiding"] = "BP:Gebiedsaanduiding"; 
+        this.wfsToWmsLayer["Bestemmingsplangebied"] = "BP:Bestemmingsplangebied";
+        this.wfsToWmsLayer["Wijzigingsplangebied"] = "BP:Wijzigingsplangebied";
+        this.wfsToWmsLayer["Enkelbestemming"] = "BP:Enkelbestemming";
+        this.wfsToWmsLayer["Figuur"] = "BP:Figuur";
+        this.wfsToWmsLayer["Lettertekenaanduiding"] = "BP:Lettertekenaanduiding";
+        this.wfsToWmsLayer["Maatvoering"] = "BP:Maatvoering";
+        this.wfsToWmsLayer["Dubbelbestemming"] = "BP:Dubbelbestemming";
+        this.wfsToWmsLayer["Bouwvlak"] = "BP:Bouwvlak";
+        this.wfsToWmsLayer["Gebiedsaanduiding"] = "BP:Gebiedsaanduiding";
         this.wfsToWmsLayer["Inpassingsplangebied"] = "BP:Inpassingsplangebied";
         this.wfsToWmsLayer["Bouwaanduiding"] = "BP:Bouwaanduiding";
         this.wfsToWmsLayer["Functieaanduiding"] = "BP:Functieaanduiding";
@@ -87,7 +87,7 @@ Ext.define("viewer.components.rotercera.IdentifyParser",{
 
         this.highlightImage = Ext.String.urlAppend(this.component.resourceUrl,"resource=resources/images/map.png");
         this.highlightedImage = Ext.String.urlAppend(this.component.resourceUrl,"resource=resources/images/map_go.png");
-        
+
         this.terceraMapping={
             enkelbestemming: [
                 {key: "agrarisch", title: "Agrarisch"},
@@ -131,7 +131,7 @@ Ext.define("viewer.components.rotercera.IdentifyParser",{
                 {key: "wetgevingzone  ", title: "Wetgevingzone  "},
                 {key: "overige_zone", title: "Overige zone"}
             ]
-        };     
+        };
     },
     resetData : function(){
         this.info="";
@@ -150,7 +150,7 @@ Ext.define("viewer.components.rotercera.IdentifyParser",{
         }
         return false;
     },
-            
+
     /**
      *Returns 1 if bestemmingsplan layout.
      */
@@ -165,13 +165,13 @@ Ext.define("viewer.components.rotercera.IdentifyParser",{
                     typePlan.indexOf("inpassingsplan")>=0 ||
                     typePlan.indexOf("wijzigingsplan")>=0 ||
                     typePlan.indexOf("uitwerkingsplan")>=0)
-                    return true;                     
+                    return true;
             }else if (layoutType==this.STRUCTUURVISIELAYOUT){
                 if(typePlan.indexOf("structuurvisie")>=0)
                     return true;
 
             }else if (layoutType==this.BESLUITGEBIED_X){
-                if (obj["besluitgebied_x"] || obj["besluitvlak_x"]){ 
+                if (obj["besluitgebied_x"] || obj["besluitvlak_x"]){
                     return true;
                 }
             }
@@ -181,7 +181,7 @@ Ext.define("viewer.components.rotercera.IdentifyParser",{
         }
         return false;
     },
-            
+
     /**
     *Make the layer head.
     */
@@ -196,7 +196,7 @@ Ext.define("viewer.components.rotercera.IdentifyParser",{
         head+="</h1>"
         return head;
     },
-            
+
     /**
     *Create the a table row with 3 columns
     */
@@ -222,7 +222,7 @@ Ext.define("viewer.components.rotercera.IdentifyParser",{
        var row="<tr>";
        for (var i=0; i < array.length; i++){
            row+="<td class=\"td"+i+"\" valign=\"top\">"+array[i]+"</td>";
-       }           
+       }
        row+="</tr>";
        return row;
     },
@@ -230,20 +230,20 @@ Ext.define("viewer.components.rotercera.IdentifyParser",{
     /**
     *Create a bestemming table row (
     */
-    createBestemmingRow : function(name,bestemmingFeatures,plangebied,sldLayer,forceIsPartOfPlan){  
+    createBestemmingRow : function(name,bestemmingFeatures,plangebied,sldLayer,forceIsPartOfPlan){
         if (forceIsPartOfPlan==undefined){
             forceIsPartOfPlan=false;
         }
         var infoPart="";
-        for (var featureCount in bestemmingFeatures){          
+        for (var featureCount in bestemmingFeatures){
             var feature=bestemmingFeatures[featureCount];
             //skip crap objects
             if (feature[null]){}
             else{
-                if (forceIsPartOfPlan ||(plangebied!=undefined && this.isPartOfPlan(feature,plangebied))){   
+                if (forceIsPartOfPlan ||(plangebied!=undefined && this.isPartOfPlan(feature,plangebied))){
                     var sldFilter=null;
                     if (feature["fid"] && sldLayer){
-                        sldFilter=this.createSLDButton(feature["fid"],sldLayer);                        
+                        sldFilter=this.createSLDButton(feature["fid"],sldLayer);
                     }
                     infoPart+=this.createRow(name,
                         feature["naam"],
@@ -254,7 +254,7 @@ Ext.define("viewer.components.rotercera.IdentifyParser",{
         }
         return infoPart;
     },
-            
+
     createSLDButton : function (id,layer){
         var origin = this.component.selectedPlan ? this.component.selectedPlan.origin : this.component.previouslySelectedPlan.origin;
         if (origin !== "Tercera" &&
@@ -270,23 +270,23 @@ Ext.define("viewer.components.rotercera.IdentifyParser",{
     /**
     *Create the links
     */
-    createLinks : function(verwijzingNaarTekst){            
+    createLinks : function(verwijzingNaarTekst){
        var linkText="";
        if (verwijzingNaarTekst && verwijzingNaarTekst!="null"){
            var links=verwijzingNaarTekst.split(",");
            for (var i=0; i < links.length; i++){
                links[i]=links[i].replace(" ","");
-               var theUrl=links[i];               
+               var theUrl=links[i];
                var typeDoc=this.component.getDocType(links[i]);
                if (linkText.length>0){
                    linkText+="<br/>";
                }
-               linkText+="<a href=\"#\" onclick=\"planDocWindow=window.open('"+theUrl+"','info',{})\">"+typeDoc+"</a><br>";
+               linkText+="<a href=\"#\" onclick=\"planDocWindow=window.open('"+theUrl+"','info','status=no,toolbar=no,menubar=no,scrollbars=yes')\">"+typeDoc+"</a><br>";
            }
        }
        return linkText;
     },
-            
+
     /**
      *Create a plan info element
      */
@@ -314,12 +314,12 @@ Ext.define("viewer.components.rotercera.IdentifyParser",{
         for (var i=0; i < data.length; i++){
             var resp = data[i];
             if (resp.features){
-                newObj[resp.request.serviceLayer] = resp.features;                
+                newObj[resp.request.serviceLayer] = resp.features;
             }
         }
         return newObj;
     },
-    makeSimpleName : function (s){            
+    makeSimpleName : function (s){
         var newS=s.toLowerCase();
         if (newS.indexOf(":")>=0){
             newS=newS.substring(newS.indexOf(":")+1);
@@ -351,11 +351,11 @@ Ext.define("viewer.components.rotercera.IdentifyParser",{
         }
         return newObj;
     },
-            
+
     /**
     *Get complex name for ROOnline layers
     */
-    getComplexName : function(complexObj,simpleName){            
+    getComplexName : function(complexObj,simpleName){
         for (var featureName in complexObj){
             if (featureName.toLowerCase().indexOf(simpleName)>=0){
                 return featureName;
@@ -373,7 +373,7 @@ Ext.define("viewer.components.rotercera.IdentifyParser",{
      */
     getComplexTerceraName : function (complexObj,simpleName){
         var simple=complexObj[simpleName];
-        if (simple){                
+        if (simple){
             var fid;
             for (var i=0; i < simple.length; i++){
                 fid=simple[i].fid;
@@ -383,46 +383,46 @@ Ext.define("viewer.components.rotercera.IdentifyParser",{
             }
             for (var featureName in complexObj){
                 //find the complex name and return
-                if (featureName!=simpleName){                        
+                if (featureName!=simpleName){
                     var complex =complexObj[featureName];
                     //check if the complex has 1 feature with fid == fid of 1 of the simple
                     for (var i=0; i < complex.length; i++){
                         if (complex[i].fid==fid){
                             return featureName;
                         }
-                    }    
+                    }
                 }
             }
         }
         return simpleName;
     },
-            
-    /*Get the correct plan that is selected*/        
+
+    /*Get the correct plan that is selected*/
     getCorrectPlan : function(planObjects,plangebied){
         for (fcount in planObjects){
             if (this.isPartOfPlan(planObjects[fcount],plangebied)){
-                return planObjects[fcount];                        
-            }                    
+                return planObjects[fcount];
+            }
         }
     },
-            
-    
+
+
     /**
      *Makes the html.
      */
-    parse : function (data,layerId,plangebied){  
+    parse : function (data,layerId,plangebied){
         var objcomplex = this.dataToObj(data);
         //remove all ':' and set all names to lowercase.
         var obj = this.makeSimple(objcomplex,layerId);
         //Zoek het juiste bestemmingsplan object
         var hetPlanObject=null;
         //if tercera layer then the info is always of the loaded plan
-        var forcePartOfPlan=false;            
+        var forcePartOfPlan=false;
         if (layerId==this.TERCERALAYERID){
             forcePartOfPlan=true;
         }
         if(obj["bestemmingsplangebied"])
-            hetPlanObject=this.getCorrectPlan(obj["bestemmingsplangebied"],plangebied);            
+            hetPlanObject=this.getCorrectPlan(obj["bestemmingsplangebied"],plangebied);
         //if still null then it's not given as a bestemmingsplangebied'
         if (hetPlanObject == null && obj["plangebied"]){
             hetPlanObject=this.getCorrectPlan(obj["plangebied"],plangebied);
@@ -434,7 +434,7 @@ Ext.define("viewer.components.rotercera.IdentifyParser",{
             hetPlanObject = this.getCorrectPlan(obj["besluitgebied_x"],plangebied);
         }
         if (hetPlanObject == null && obj["besluitvlak_x"]){
-            hetPlanObject = this.getCorrectPlan(obj["besluitvlak_x"],plangebied);                
+            hetPlanObject = this.getCorrectPlan(obj["besluitvlak_x"],plangebied);
         }
         if (hetPlanObject!=null && this.hasInfoWithLayout(obj,hetPlanObject,this.BESTEMMINGSPLANLAYOUT)){
             var infoPart="";
@@ -448,7 +448,7 @@ Ext.define("viewer.components.rotercera.IdentifyParser",{
                 //maybe it's a tercera feature?
                 for (var i=0; i < this.terceraMapping.enkelbestemming.length; i++){
                     var mapping = this.terceraMapping.enkelbestemming[i];
-                    if (obj[mapping.key]){                            
+                    if (obj[mapping.key]){
                         infoPart+=this.createBestemmingRow("Enkelbestemming",obj[mapping.key],plangebied,this.getComplexTerceraName(objcomplex,mapping.key),forcePartOfPlan);
                     }
                 }
@@ -463,7 +463,7 @@ Ext.define("viewer.components.rotercera.IdentifyParser",{
                     }
                 }
             }
-            if (obj["gebiedsaanduiding"]){ 
+            if (obj["gebiedsaanduiding"]){
                 infoPart+=this.createBestemmingRow("Gebiedsaanduiding",obj["gebiedsaanduiding"],plangebied,this.getComplexName(objcomplex,"gebiedsaanduiding"),forcePartOfPlan);
             }else if (layerId==this.TERCERALAYERID){
                 for (var i=0; i < this.terceraMapping.gebiedsaanduiding.length; i++){
@@ -473,7 +473,7 @@ Ext.define("viewer.components.rotercera.IdentifyParser",{
                     }
                 }
             }
-            if (obj["bouwvlak"]){  
+            if (obj["bouwvlak"]){
                 for (var featureCount in obj["bouwvlak"]){
                     var feature=obj["bouwvlak"][featureCount];
                     if (forcePartOfPlan || this.isPartOfPlan(feature,plangebied))
@@ -482,12 +482,12 @@ Ext.define("viewer.components.rotercera.IdentifyParser",{
                         }
                 }
             }
-            if (obj["bouwaanduiding"]){          
+            if (obj["bouwaanduiding"]){
                 infoPart+=this.createBestemmingRow("Bouwaanduiding",obj["bouwaanduiding"],plangebied,null,forcePartOfPlan);
 
             }
             if (obj["maatvoering"]){
-                for (var featureCount in obj["maatvoering"]){    
+                for (var featureCount in obj["maatvoering"]){
                     var feature=obj["maatvoering"][featureCount];
                     if (forcePartOfPlan || this.isPartOfPlan(feature,plangebied)){
                         var newValue="";
@@ -509,13 +509,13 @@ Ext.define("viewer.components.rotercera.IdentifyParser",{
                             /*feature["naam"]+*/newValue);
                     }
 
-                }                    
+                }
             }
             if (obj["functieaanduiding"]){
                 infoPart+=this.createBestemmingRow("Functieaanduiding",obj["functieaanduiding"],plangebied,this.getComplexName(objcomplex,"functieaanduiding"),forcePartOfPlan);
             }
             if (obj["figuur"]){
-                for (var featureCount in obj["figuur"]){    
+                for (var featureCount in obj["figuur"]){
                     var feature=obj["figuur"][featureCount];
                     if (forcePartOfPlan || this.isPartOfPlan(feature,plangebied)){
                         infoPart+=this.createRow("figuur",
@@ -533,17 +533,17 @@ Ext.define("viewer.components.rotercera.IdentifyParser",{
             var infoPart="";
 
             infoPart+=this.makeLayerHead(hetPlanObject);
-            infoPart+="<table class='featureinfo-table'>"; 
+            infoPart+="<table class='featureinfo-table'>";
             infoPart+=this.createBestemmingRow("Structuurvisie",obj["functieaanduiding"],plangebied);
-            infoPart+="</table>";                
+            infoPart+="</table>";
 
             infoPart+=this.createPlanInfo(hetPlanObject);
 
-            this.info=infoPart;                
+            this.info=infoPart;
         }else if(hetPlanObject!=null && this.hasInfoWithLayout(obj,hetPlanObject,this.BESLUITGEBIED_X)){
             var infoPart="";
-            infoPart+=this.makeLayerHead(hetPlanObject);                
-            infoPart+="<table class='featureinfo-table'>";                
+            infoPart+=this.makeLayerHead(hetPlanObject);
+            infoPart+="<table class='featureinfo-table'>";
             if (obj["besluitvlak_x"]){
                 infoPart+=this.createBestemmingRow("Besluitvlak",obj["besluitvlak_x"],plangebied,this.getComplexName(objcomplex,"besluitvlak_x"),forcePartOfPlan);
             }
@@ -564,18 +564,18 @@ Ext.define("viewer.components.rotercera.IdentifyParser",{
                     if(feature["openbaar"]=="true"){
                         openbaar=true;
                     }
-                    var eigenaar=feature["eigenaar"];                    
+                    var eigenaar=feature["eigenaar"];
                     if(openbaar || username == eigenaar || (isUtrechtOrg(username) && isUtrechtOrg(eigenaar))){
                         commentaarInfoPart+=this.createRow(eigenaar,feature["thema"],feature["tekst"]);
                         var owner=feature["eigenaar"];
                         if (username == owner /*|| (isUtrechtOrg(username) && isUtrechtOrg(owner))*/){
                             var wijzigUrl=commentaarUrl+"?id="+feature["id"];
-                            commentaarInfoPart+=this.createRow("<a href=\"javascript: var d=flamingoWindow.openPlanCommentaar('"+wijzigUrl+"');\">Wijzig</a>" );                        
+                            commentaarInfoPart+=this.createRow("<a href=\"javascript: var d=flamingoWindow.openPlanCommentaar('"+wijzigUrl+"');\">Wijzig</a>" );
                         }
                         count++;
                     }
                 }
-            }     
+            }
             commentaarInfoPart+="</table>"
             if (count>0){
                 this.commentaarInfo+=commentaarInfoPart;
@@ -587,16 +587,16 @@ Ext.define("viewer.components.rotercera.IdentifyParser",{
                     var head=this.makeLayerHead(hetPlanObject);
                     if (head)
                         infoPart+=head;
-                }catch(err){                        
-                }                    
+                }catch(err){
+                }
             }
             infoPart+=this.createPlanInfo(hetPlanObject);
             this.info+=infoPart;
         }
         return this.info;
 
-    },   
+    },
     getInfo : function(){
         return this.info+this.commentaarInfo;
-    }      
+    }
 });
